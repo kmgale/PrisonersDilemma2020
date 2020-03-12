27787e95606ab72ff070328a4d57b47fd44a0be6
jlobbia <61804724+jlobbia@users.noreply.github.com>
2020-03-12 12:41:49 -0700

    Update team17.py

--- team17.py
+++ team17.py
@@ -1,1 +1,16 @@
-asdf
+team_name = 'Team 16'
+strategy_name = 'friendly is just the tip of the iceberg :)'
+strategy_description = 'Betray if other team betray. Collude if other team colludes.'
+
+def move(my_history, their_history, my_score, their_score):
+  import random
+  b=0
+  c=0
+  if 'b' in their_history:
+    b+=1
+  if 'c' in their_history:
+    c+=1
+  if int(b)>=int(c):
+    return 'b'
+  if int(c)<int(b):
+    return 'c'
